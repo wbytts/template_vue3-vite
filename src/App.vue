d401b86ce3ca8fb7cb74400995a272dbcafb3618
wbytts <wbytts@163.com>
2024-09-22 21:30:05 +0800

    feat: 配置 unocss

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { ElConfigProvider } from 'element-plus'
+import type { ConfigProviderProps } from 'element-plus'
+
+// Element Plus 全局配置对象
+const elementConfig = {
+  size: 'small',
+  zIndex: 3000
+} as ConfigProviderProps
+
 </script>
 
 <template>
-  <RouterView />
+  <el-config-provider v-bind="elementConfig">
+    <RouterView />
+  </el-config-provider>
 </template>
 
 <style scoped></style>
--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,49 @@
-import { defineConfig } from 'unocss'
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+  presetWebFonts,
+  presetTagify,
+  transformerDirectives,
+  transformerVariantGroup,
+  transformerCompileClass,
+  transformerAttributifyJsx
+} from 'unocss'
+import presetRemToPx from '@unocss/preset-rem-to-px'
+
+// 交互式文档： https://unocss.dev/interactive/
+// TailwindCSS： https://www.tailwindcss.cn/
 
 export default defineConfig({
-  // ...UnoCSS options
+  shortcuts: [
+    // ...
+  ],
+  theme: {
+    colors: {
+      // ...
+    }
+  },
+  presets: [
+    presetRemToPx({ baseFontSize: 16 }), // 1rem == 16px
+    presetUno(), // == @unocss/preset-wind + @unocss/preset-mini
+    presetAttributify(), // 属性化预设
+    presetIcons(), // 图标
+    presetTypography(), // 排版
+    presetWebFonts({
+      fonts: {
+        // ...
+      }
+    }),
+    presetTagify({ prefix: 'uno-' })
+  ],
+  transformers: [
+    transformerVariantGroup(), // 变体组
+    transformerDirectives(), // 指令
+    transformerCompileClass(),
+    transformerAttributifyJsx({
+      blocklist: [/text-[a-zA-Z]*/, 'text-5xl']
+    })
+  ]
 })
