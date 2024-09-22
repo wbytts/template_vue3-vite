ff325e2f24850463c7e5c08c8608ea1197750aee
wbytts <wbytts@163.com>
2024-09-22 21:01:13 +0800

    feat: 添加 element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'virtual:uno.css'
 import './styles/global.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +15,13 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    }),
+    UnoCSS()
   ],
   resolve: {
     alias: {
