6ca8148d97cb981d096e82b174cc4f0c95daf12c
wbytts <wbytts@163.com>
2024-09-22 21:41:17 +0800

    feat: 重置全局样式

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import 'virtual:uno.css'
-import './styles/global.scss'
+import '@unocss/reset/tailwind-compat.css' // Tailwind兼容版
+import 'virtual:uno.css' // unocss 虚拟文件
+import './styles/global.scss' // 全局样式
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
